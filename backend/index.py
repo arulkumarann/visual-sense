54cc84f456971e57207581b9dc401f250a02a813
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-03-16 10:14:37 +0530

    refactor: refactor the prediction logic

--- backend/index.py
+++ backend/index.py
@@ -20,17 +20,6 @@ async def test(req: Request):
     return JSONResponse({"success": "Website Loaded Successfully"})
 
 #Route to upload files
-@app.post("/upload")
-async def upload(file_uploads:list[UploadFile]):
-    filenames=[]
-    for file_upload in file_uploads:
-        data=await file_upload.read()
-        save_file=file_upload.filename
-        with open(save_file,"wb") as f:
-            f.write(data)
-        filenames.append(save_file)
-        return JSONResponse({"Filenames":filenames})
-    
 @app.post("/predict")
 async def predict(text: str, image_file: UploadFile = File(...)):
     with open("temp_image.jpg", "wb") as buffer:
